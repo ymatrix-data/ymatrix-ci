73d23f4ccd27983f46be7de403ea0974f709e14f
Wang Hao <wanghao@ymatrix.cn>
2021-07-08 15:52:26 +0800

    Implement MatrixDB-ICW pipeline

---
+++ README.md
@@ -0,0 +1,10 @@
+MatrixDB Public CI
+---
+
+[![MatrixDB_ICW](https://github.com/ymatrix-data/ymatrix-ci/actions/workflows/icw.yml/badge.svg)](https://github.com/ymatrix-data/ymatrix-ci/actions/workflows/icw.yml)
+
+[![MatrixDB_4X_ICW](https://github.com/ymatrix-data/ymatrix-ci/actions/workflows/icw_4x.yml/badge.svg)](https://github.com/ymatrix-data/ymatrix-ci/actions/workflows/icw_4x.yml)
+
+
+Own and Maintained by [ymatrix.cn](https://ymatrix.cn)\
+北京四维纵横数据技术有限公司
---
+++ README.ubuntu.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+  bison \
+  ccache \
+  cmake \
+  curl \
+  flex \
+  git-core \
+  gcc \
+  g++ \
+  inetutils-ping \
+  krb5-kdc \
+  krb5-admin-server \
+  libapr1-dev \
+  libbz2-dev \
+  libcurl4-gnutls-dev \
+  libevent-dev \
+  libkrb5-dev \
+  libpam-dev \
+  libperl-dev \
+  libreadline-dev \
+  libssl-dev \
+  libxerces-c-dev \
+  libxml2-dev \
+  libyaml-dev \
+  libzstd-dev \
+  locales \
+  net-tools \
+  ninja-build \
+  openssh-client \
+  openssh-server \
+  openssl \
+  python3-dev \
+  python3-pip \
+  python3-psutil \
+  python3-pygresql \
+  python3-setuptools \
+  python3-yaml \
+  zlib1g-dev
+
+pip3 install conan
